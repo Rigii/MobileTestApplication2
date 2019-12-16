c902b0a60eae29c23bdfbde6b02889dce7fa1afe
Rigii <jacobne@list.ru>
2019-12-16 22:11:39 +0200

    save to storage

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -2,8 +2,21 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './reducer';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage';
+//import storage from 'redux-persist/lib/storage'
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const persistConfig = {
+  key: 'mobile_app',
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+export const persistor = persistStore(store)
+
+// export const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
