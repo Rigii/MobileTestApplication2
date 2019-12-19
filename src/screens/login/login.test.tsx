bdefb4a6bef9c5efd19c1f3bc0269f3d0f97c833
user <user@iMac-user.local>
2019-12-19 19:47:24 +0200

    tests commits

--- src/screens/login/login.test.tsx
+++ src/screens/login/login.test.tsx
@@ -9,22 +9,22 @@ describe('Login Screen Component', () => {
       getTodoList: jest.fn(),
       setEmail: jest.fn(),
     };
-    const {asJSON} = render(<LoginScreenComp {...props} />);
+    const {asJSON} = render(<LoginScreenComp {...props} />); // saving screen snaphot structure as json in __snapshots__
 
-    expect(asJSON()).toMatchSnapshot(); // snapshot settings 
+    expect(asJSON()).toMatchSnapshot(); // checking is current structure are mached with saved snapshot
   });
 
   it('Should contain email input and submit button', () => {
-    const props = {
+    const props = { // mocking props
         getTodoList: jest.fn(),
         setEmail: jest.fn(),
     };
     const {getByTestId} = render(<LoginScreenComp {...props} />); // return pseudorender marcup structure 
 
-    const input = getByTestId('loginInput'); // getting feld
-    const button = getByTestId('loginButton');
+    const input = getByTestId('loginInput'); // getting feld input from getByTestId 
+    const button = getByTestId('loginButton'); // getting button ...
 
-    expect(input).toBeTruthy(); // checking is input is exist
+    expect(input).toBeTruthy(); // checking is input exist
     expect(button).toBeTruthy()
   });
 
@@ -39,9 +39,11 @@ describe('Login Screen Component', () => {
     const input = getByTestId('loginInput');
     const button = getByTestId('loginButton');
 
-      fireEvent.changeText(input, 'famousWomanInHistory')
-      fireEvent.press(button)
+      fireEvent.changeText(input, 'famousWomanInHistory') // setting mocked text to input
+      fireEvent.press(button) // pressing but
 
       expect(props.setEmail).toHaveBeenCalledWith('famousWomanInHistory') // is function setEmail was called with mocked arguments 
   })
 });
+
+// TODO: Write tests for other component functions
